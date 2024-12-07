d3b3a2c69b40d6972de8653b0fd8a26a07195c6d
Junaid Bashir <junaid@servicemarket.com>
2024-12-07 08:46:17 +0500

    Some new test cases

--- app/src/test/java/com/example/androidtesting/calculator/CalculatorTest.kt
+++ app/src/test/java/com/example/androidtesting/calculator/CalculatorTest.kt
@@ -1,13 +1,15 @@
 package com.example.androidtesting.calculator
 
 import com.nhaarman.mockitokotlin2.mock
+import org.junit.Assert
 import org.junit.Assert.*
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 
 class CalculatorTest{
-
 
 
     @Test
@@ -18,6 +20,18 @@ class CalculatorTest{
         val result = calculator.add(5,3)
         assertEquals(8,result)
 
+    }
+
+    @Test
+    fun subtract2Numbers(){
+
+        val calculator = mock<Calculator>()
+        val myCalculator = MyCalculator(calculator)
+
+        Mockito.`when`(myCalculator.subtract(10,2)).thenReturn(7)
+        val result = myCalculator.subtract(10,2)
+
+        Assert.assertEquals(result,8)
     }
 
     @Test
